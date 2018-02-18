b35e716779d99b6a0d9e52d762185e8b5592852a
David Francisco <dmfrancisc@gmail.com>
2018-02-18 11:14:05 +0000

    Step 4: Fetching and rendering markdown

--- src/data.js
+++ src/data.js
@@ -1,12 +1,14 @@
 export default {
   posts: {
     "creating-blog-with-cra-and-github": {
+      gist: "f4f5311ad2ec25147bc458d791fdaeb5",
       date: "2018-02-18",
       title: "Creating a blog with create-react-app and GitHub",
       summary:
         "Create React App is a great tool that lets you start a new React application very easily. There are some limitations though that you need to be aware of.",
     },
     "dear-hume": {
+      gist: "150ed6aa20f9b72ef3fcaf39ac2f89c6",
       date: "1958-04-22",
       title: "Dear Hume",
       summary:
